c5904773688fd011f34aa9a4dfaf0da674684672
Carles Pagès <page@cubata.homelinux.net>
2010-10-23 00:00:32 +0200

    Adding a unified FindSDL cmake module and a toolchain file for the Wii. Everything is still broken!

---
+++ cmake/FindSDL.cmake
@@ -0,0 +1,140 @@
+# clean result
+set (SDL_FOUND false)
+set (SDL_INCLUDE_DIRS "")
+set (SDL_LIBRARIES "")
+
+# use pkg-config to get hints
+include (LibFindMacros)
+libfind_pkg_check_modules (SDL_PKGCONF sdl)
+
+set (COMMON_SEARCH_PATHS
+  /sw # Fink
+  /opt/local # DarwinPorts
+  /opt/csw # Blastwave
+  /opt)
+
+set (INC_SEARCH_PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/include/SDL12
+  /usr/local/include/SDL11 # FreeBSD ports
+  /usr/include/SDL12
+  /usr/include/SDL11)
+
+find_path (SDL_INCLUDE_DIR SDL.h
+  HINTS $ENV{SDLDIR}
+  PATH_SUFFIXES include/SDL include
+  PATHS ${SDL_PKGCONF_INCLUDE_DIRS}
+  ${INC_SEARCH_PATHS}
+  ${COMMON_SEARCH_PATHS})
+MESSAGE ("SDL_INCLUDE_DIR is ${SDL_INCLUDE_DIR}")
+
+# SDL-1.1 is the name used by FreeBSD ports...
+# don't confuse it for the version number.
+find_library (SDL_LIB 
+  NAMES SDL SDL-1.1
+  HINTS $ENV{SDLDIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS ${SDL_PKGCONF_LIBRARY_DIRS}
+  ${COMMON_SEARCH_PATHS})
+MESSAGE ("SDL_LIB is ${SDL_LIB}")
+
+if (NOT SDL_BUILDING_LIBRARY)
+  if (NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
+    # Non-OS X framework versions expect you to also dynamically link to 
+    # SDLmain. This is mainly for Windows and OS X. Other (Unix) platforms 
+    # seem to provide SDLmain for compatibility even though they don't
+    # necessarily need it.
+    find_library (SDLMAIN_LIBRARY 
+      NAMES SDLmain SDLmain-1.1
+      HINTS $ENV{SDLDIR}
+      PATH_SUFFIXES lib64 lib
+      PATHS ${SDL_PKGCONF_LIBRARY_DIRS}
+      ${COMMON_SEARCH_PATHS})
+  endif ()
+endif ()
+
+# SDL may require threads on your system.
+# The Apple build may not need an explicit flag because one of the 
+# frameworks may already provide it. 
+# But for non-OSX systems, I will use the CMake Threads package.
+if (NOT APPLE)
+  find_package (Threads)
+endif ()
+
+# MinGW needs an additional library, mwindows
+# It's total link flags should look like -lmingw32 -lSDLmain -lSDL -lmwindows
+# (Actually on second look, I think it only needs one of the m* libraries.)
+if (MINGW)
+  set (MINGW32_LIBRARY mingw32 CACHE STRING "mwindows for MinGW")
+endif ()
+
+macro (FIND_LIBS _LIBS)
+  foreach (LIB ${${_LIBS}})
+    MESSAGE ("Searching for SDL_${LIB}")
+    libfind_pkg_check_modules (SDL_${LIB}_PKGCONF SDL_${LIB})
+    find_library (SDL_${LIB}
+      NAMES SDL_${LIB}
+      HINTS $ENV{SDLDIR}
+      PATH_SUFFIXES lib64 lib
+      PATHS ${SDL_${LIB}_PKGCONF_LIBRARY_DIRS}
+      ${COMMON_SEARCH_PATHS})
+    MESSAGE ("SDL_LIBRARY is ${SDL_${LIB}}")
+    mark_as_advanced (SDL_${LIB})
+  endforeach ()
+endmacro ()
+
+FIND_LIBS (SDL_FIND_COMPONENTS)
+
+set (HARL /home/page)
+
+set (SDL_PROCESS_INCLUDES SDL_INCLUDE_DIR HARL)
+set (SDL_PROCESS_LIBS SDL_LIB)
+foreach (LIB ${SDL_FIND_COMPONENTS})
+  list (APPEND SDL_PROCESS_LIBS SDL_${LIB})
+endforeach ()
+
+MESSAGE ("PL is ${SDL_PROCESS_LIBS}")
+
+libfind_process(SDL)
+
+#if (SDL_LIBRARY_TEMP)
+#  # For SDLmain
+#  IF(NOT SDL_BUILDING_LIBRARY)
+#    IF(SDLMAIN_LIBRARY)
+#      SET(SDL_LIBRARY_TEMP ${SDLMAIN_LIBRARY} ${SDL_LIBRARY_TEMP})
+#    ENDIF(SDLMAIN_LIBRARY)
+#  ENDIF(NOT SDL_BUILDING_LIBRARY)
+#
+#  # For OS X, SDL uses Cocoa as a backend so it must link to Cocoa.
+#  # CMake doesn't display the -framework Cocoa string in the UI even 
+#  # though it actually is there if I modify a pre-used variable.
+#  # I think it has something to do with the CACHE STRING.
+#  # So I use a temporary variable until the end so I can set the 
+#  # "real" variable in one-shot.
+#  IF(APPLE)
+#    SET(SDL_LIBRARY_TEMP ${SDL_LIBRARY_TEMP} "-framework Cocoa")
+#  ENDIF(APPLE)
+#    
+#  # For threads, as mentioned Apple doesn't need this.
+#  # In fact, there seems to be a problem if I used the Threads package
+#  # and try using this line, so I'm just skipping it entirely for OS X.
+#  IF(NOT APPLE)
+#    SET(SDL_LIBRARY_TEMP ${SDL_LIBRARY_TEMP} ${CMAKE_THREAD_LIBS_INIT})
+#  ENDIF(NOT APPLE)
+#
+#  # For MinGW library
+#  IF(MINGW)
+#    SET(SDL_LIBRARY_TEMP ${MINGW32_LIBRARY} ${SDL_LIBRARY_TEMP})
+#  ENDIF(MINGW)
+#
+#  # Set the final string here so the GUI reflects the final state.
+#  SET(SDL_LIBRARY ${SDL_LIBRARY_TEMP} CACHE STRING "Where the SDL Library can be found")
+#  # Set the temp variable to INTERNAL so it is not seen in the CMake GUI
+#  SET(SDL_LIBRARY_TEMP "${SDL_LIBRARY_TEMP}" CACHE INTERNAL "")
+#
+#  SET(SDL_FOUND "YES")
+#ENDIF(SDL_LIBRARY_TEMP)
+
+#MESSAGE("SDL_LIBRARY is ${SDL_LIBRARY}")
+
---
+++ cmake/toolchain-powerpc-eabi.cmake
@@ -0,0 +1,17 @@
+set (CMAKE_SYSTEM_NAME Generic)
+set (CMAKE_SYSTEM_VERSION 1)
+
+set (DEVKITPPC $ENV{DEVKITPPC})
+set (DEVKITPRO $ENV{DEVKITPRO})
+
+set (CMAKE_CXX_COMPILER ${DEVKITPPC}/bin/powerpc-eabi-g++)
+set (CMAKE_C_COMPILER ${DEVKITPPC}/bin/powerpc-eabi-gcc)
+
+# where is the target environment 
+set (CMAKE_FIND_ROOT_PATH  ${DEVKITPPC}
+    ${DEVKITPRO}/libogc
+    ${DEVKITPRO}/portlibs/ppc)
+
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
